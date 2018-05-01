239fc0510d3f528bc4759ef69fdcb9fe54d10974
Eric Renart <eric.renart@stonybrook.edu>
2018-04-30 21:53:30 -0400

    implement rest of sounds and a grace period after hand is captured in which the claw won't snap shut

--- DionaeaMuscipula/DionaeaMuscipula/RobotThread.cs
+++ DionaeaMuscipula/DionaeaMuscipula/RobotThread.cs
@@ -11,18 +11,21 @@ namespace _DionaeaMuscipula
     class RobotThread
     {
         bool isActive;
-        bool isRecording;
+        bool clawLock;
+        int lockElapsed = 0;
+        int begTimerElapsed = 0;
         Brick<TouchSensor, NXTLightSensor, NXTLightSensor, Sonar> nxt;
-        Form1 f1; // this is to update the readout
-        const int lightdiff = 2; //difference between light sensor values in order for the arm to move in a direction
-        const int sonarThreshold = 24; // minimum distance in centimenters the hand (or other object) must be from the claw in order for it to snap shut
+        const int LIGHT_DIFF = 2; //difference between light sensor values in order for the arm to move in a direction
+        const int SONAR_THRESHOLD = 24; // minimum distance in centimenters the hand (or other object) must be from the claw in order for it to snap shut
+        const int CLAW_LOCK_INTERVAL = 5000; // lock the claw for ~5 sec after hand is freed via touch sensor
+        const int BEG_INTERVAL = 30000; // beg for human contact every 30s by playing an rso
 
         public RobotThread(Brick<TouchSensor, NXTLightSensor, NXTLightSensor, Sonar> nxti, Form1 f1i)
         {
             nxt = nxti;
             f1 = f1i;
             isActive = true;
-            isRecording = false;
+            clawLock = false;
         }
 
         public void start()
@@ -42,36 +45,39 @@ namespace _DionaeaMuscipula
             // Pressing the K key kills everything if things go awry
             while (isActive)
             {
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                // read sensor data
                 int light1 = nxt.Sensor2.ReadLightLevel();
                 int light2 = nxt.Sensor3.ReadLightLevel();
                 int sonar = nxt.Sensor4.ReadDistance();
 
+                // for debugging purposes
                 Console.WriteLine("["+light1+"]---["+sonar+" cm]---["+light2+"]");
 
                 // if the left light sensor reading exceeds the right light sensor reading, move right
-                if (light1 - light2 >= lightdiff)
+                if (light1 - light2 >= LIGHT_DIFF)
                 {
                     nxt.MotorA.On(-30);
                 }
 
                 // if the right light sensor reading exceeds the left light sensor reading, move left
-                if(light2 - light1 >= lightdiff)
+                if(light2 - light1 >= LIGHT_DIFF)
                 {
                     nxt.MotorA.On(30);
                 }
 
                 
                 // if the difference between sensor values is less than the threshold stop the motor
-                if(Math.Abs(light2 - light1) <= lightdiff)
+                if(Math.Abs(light2 - light1) <= LIGHT_DIFF)
                 {
                     nxt.MotorA.Off();
                 }
 
-                if(sonar < sonarThreshold)
+                // Capture the hand if it is within the capture interval and the claw is not locked.
+                if(sonar < SONAR_THRESHOLD && !clawLock)
                 {
                     nxt.MotorC.On(-50);
                     nxt.PlaySoundFile("cannotescape.rso",false);
+                    ArmWrestle(30000);
                 }
 
                 // release the hand if the touch sensor is pressed
@@ -84,23 +90,46 @@ namespace _DionaeaMuscipula
                     nxt.MotorC.On(-50);
                     Thread.Sleep(1000);
                     nxt.MotorC.Off();
+                    clawLock = true;
                 }
 
                 // kill key
                 if(Console.Read() == 'k')
                 {
                     Console.WriteLine("Kill command issued.");
+                    isActive = false;
                     break;
+                }
+
+                // 5 second grace period after the touch sensor is pressed (assuming this loop takes ~1ms to step)
+                if(clawLock)
+                {
+                    lockElapsed++;
+                    if(lockElapsed >= CLAW_LOCK_INTERVAL)
+                    {
+                        clawLock = false;
+                        lockElapsed = 0;
+                    }
+                }
+
+                // beg for human contact every 30s or so
+                begTimerElapsed++;
+                if(begTimerElapsed >= BEG_INTERVAL)
+                {
+                    nxt.PlaySoundFile("isanybodyaround.rso", false);
+                    begTimerElapsed = 0;
                 }
             }
 
             // --------- End Main Loop -------------
         }
 
-        public void ArmWrestle()
+        public void ArmWrestle(int duration)
         {
             // moves in different directions once the arm is captured
             nxt.PlaySoundFile("chicken.rso",false);
+            nxt.PlaySoundFile("bonecrack.rso", true);
+
         }
 }
 }
